a8d1ab4a46969fdcf1dc79a15e053bcb9fe306b1
juamngie <82877810+juamngie@users.noreply.github.com>
2023-04-02 11:30:26 +0800

    Update DecimalToSignedBinary.py

--- DecimalToSignedBinary.py
+++ DecimalToSignedBinary.py
@@ -1,7 +1,9 @@
 decimal = input("Decimal: ")
 decimal_int = int(decimal)
 
-binary= bin(abs(decimal_int))[2:]
+#This line calculates the absolute value of decimal_int, converts it to binary using the built-in bin() function, and removes the prefix "0b" 
+#by slicing from the 3rd character onward (i.e., [2:]). The resulting binary value is assigned to the binary variable as a string.
+binary= bin(abs(decimal_int))[2:] #convert absolute value of decimal to binary
 
 def valid_binary(input):
     # check if input is valid binary number
@@ -21,29 +23,40 @@ def valid_binary(input):
 def decToBin(num):
     return bin(abs(num))[2:]
 
+#This is a function that takes a binary string num and calculates its two's complement. 
+#It first initializes a boolean variable foundOne to False. 
+#Then it loops through the num string in reverse order using the reversed() and range() functions. 
+#If it encounters a '1' digit and foundOne is False, it sets foundOne to True. If it encounters a '1' 
+#digit and foundOne is True, it changes the digit to '0'. If it encounters a '0' digit and foundOne is True, it changes the digit to '1'.
 def twosComp(num):
-    foundOne = 0 # boolean
+    #convert binary string to list of integers
+    binary_list = list(map(int, num))
     
-    for x in reversed(range(len(num))):
-        if binary[x] == 1 and foundOne == 0:
-            foundOne = 1
-        elif binary[x] == 1 and foundOne == 1:
-            binary[x] = 0
-        elif binary[x] == 0 and foundOne == 1:
-            binary[x] = 1
+    foundOne = False # boolean
+     # iterate through binary digits in reverse order
+    for x in reversed(range(len(binary_list))):
+        if binary_list[x] == 1 and not foundOne:
+            foundOne = True
+        elif binary_list[x] == 1 and foundOne:
+            binary_list[x] = 0
+        elif binary_list[x] == 0 and foundOne:
+            binary_list[x] = 1
+            
+    # convert list of integers back to binary string
+    return ''.join(map(str, binary_list))
 
 options = {
     "1": (decToBin, "Decimal to binary"),
-    "2": (binToDec, "Binary to decimal"),
+    #"2": (binToDec, "Binary to decimal"), not yet made
     "3": (twosComp, "Two's complement")
 }
 
 mode = input("Mode (1: Decimal to binary, 2: Binary to decimal, 3: Two's complement): ")
 
-if decimal_int < 0: # input is negative
-    decToBin(abs(decimal_int)) # get positive of input
-    twosComp(binary)
-else: # input is positive
-    decToBin(decimal_int)
+if decimal_int < 0:  # input is negative
+    binary = decToBin(abs(decimal_int))  # get positive of input
+    binary = twosComp(binary)
+else:  # input is positive
+    binary = decToBin(decimal_int)
 
 print(binary)
