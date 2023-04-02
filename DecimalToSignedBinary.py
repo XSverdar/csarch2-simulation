17e96959039e608033500d6fb5e3ad8f31a5a6a6
juamngie <82877810+juamngie@users.noreply.github.com>
2023-04-02 11:16:19 +0800

    Update DecimalToSignedBinary.py

--- DecimalToSignedBinary.py
+++ DecimalToSignedBinary.py
@@ -1,11 +1,25 @@
-decimal = int(input("Decimal: "))
+decimal = input("Decimal: ")
+decimal_int = int(decimal)
 
-binary = [0] # initialize list
+binary= bin(abs(decimal_int))[2:]
+
+def valid_binary(input):
+    # check if input is valid binary number
+    if not all(char in '01' for char in input):
+        return False
+    # check if input is integer (either binary or decimal format)
+    try:
+        int(input, 2)
+        return True
+    except ValueError:
+        try:
+            int(input)
+            return True
+        except ValueError:
+            return False
 
 def decToBin(num):
-    if num >= 1: # to not exceed maximum recursion depth
-        decToBin(num//2)
-        binary.append(num%2)
+    return bin(abs(num))[2:]
 
 def twosComp(num):
     foundOne = 0 # boolean
@@ -17,11 +31,19 @@ def twosComp(num):
             binary[x] = 0
         elif binary[x] == 0 and foundOne == 1:
             binary[x] = 1
-        
-if decimal < 0: # input is negative
-    decToBin(abs(decimal)) # get positive of input
+
+options = {
+    "1": (decToBin, "Decimal to binary"),
+    "2": (binToDec, "Binary to decimal"),
+    "3": (twosComp, "Two's complement")
+}
+
+mode = input("Mode (1: Decimal to binary, 2: Binary to decimal, 3: Two's complement): ")
+
+if decimal_int < 0: # input is negative
+    decToBin(abs(decimal_int)) # get positive of input
     twosComp(binary)
 else: # input is positive
-    decToBin(decimal)
-    
+    decToBin(decimal_int)
+
 print(binary)
